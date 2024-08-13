8cb30be17c7c7b94fa7290b7d1e6ccafdd56b43a
edalluka <153393219+edalluka@users.noreply.github.com>
2024-08-13 11:45:07 +0200

    Add files via upload

---
+++ DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+    @GetMapping("/hello/number")
+    public int helloNumber(@RequestParam(value = "number", defaultValue = "6") int number) {
+        return 8 + number;
+    }
+}
+
+
